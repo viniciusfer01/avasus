d1e4692d6a4fdd3aa7c6fc22cc1b7cd85487241e
vinicius <business.fernandes22@gmail.com>
2023-01-31 19:55:42 -0300

    Implementação do PieChart

--- components/UsuariosPorCurso.jsx
+++ components/UsuariosPorCurso.jsx
@@ -1,4 +1,15 @@
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+
 const { Container, Heading, Text } = require("@chakra-ui/react");
+
+const data = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+
+const COLORS = ["#2F2E41", "#FFFFFF", "#707070", "#D2202C"];
 
 const UsuariosPorCurso = () => {
   return (
@@ -12,6 +23,26 @@ const UsuariosPorCurso = () => {
       <Heading fontSize={"1.5rem"} textColor={"#D2202C"} padding={"2rem 0"}>
         Usuários por curso
       </Heading>
+
+      <ResponsiveContainer width={"100%"} height={"60%"}>
+        <PieChart width={800} height={800}>
+          <Pie
+            data={data}
+            dataKey="value"
+            cx="50%"
+            cy="40%"
+            outerRadius={100}
+            fill="#8884d8"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
 
       <Text>Curso de prevenção ao suicídio: 43.412</Text>
       <Text>A covid-19 e seus sintomas: 120.000</Text>
